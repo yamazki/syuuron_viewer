7a6981b6e29999c2876602142fb7659c030b16d9
yamazaki <ma17123@shibaura-it.ac.jp>
2018-12-02 00:12:44 +0900

    グラフの動的変化実装

---
+++ src/renderer/components/Bar.vue
@@ -0,0 +1,23 @@
+<script>
+import { Bar, mixins } from 'vue-chartjs'
+export default {
+  extends: Bar,
+  mixins: [mixins.reactiveProp],
+  props: ['charData', 'options'],
+
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  },
+  
+  watch: {
+    chartData: function() {
+      this.renderChart(this.chartData, this.options)
+    },
+    options: function() {
+      this.renderChart(this.chartData, this.options)
+    },
+    deep: true
+  },
+  
+}
+</script>
--- src/renderer/components/Index.vue
+++ src/renderer/components/Index.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-    <div class="main">
-      <h1>Hello electron-vue. My name is {{ name }}</h1>
+    <div class="sideBar">
+      <ul>
+        <li v-for="func in functionList">{{ func.function.functionName.replace(">","") }}</li>
+      </ul>
     </div>
+    <div class="graph">
+      <bar ref="bar" :chartData="datacollection" :options="options" :height="100" />
+    </div>
+    <button v-on:click="pu"> test</button>
   </div>
 </template>
 
@@ -10,27 +16,96 @@
 import fs from 'fs'
 import util from 'util'
 import parser from 'xml2json-light'
+import Bar from '@/components/Bar'
+
 
 export default {
-  data () {
-    return {
-      name: 'hoge'
+  
+  components: {
+    Bar,
+  },
+  
+  
+  methods: {
+    pu: function() {
+      this.datacollection.labels.push("test");
+      console.table(this.datacollection.labels);
+      this.$refs.bar.renderChart(this.datacollection, this.options);
     }
   },
-  mounted:function () {
-    const filePath = 'C:/Users/ma17123/source/repos/syuuron/syuuron/XML/温度データ保存機能.xml';
-    util.promisify(fs.readFile)(filePath, {encoding : 'utf8'})
-        .then(data => console.table(parser.xml2json(data)))
-        .catch(err => console.log(err));
+  
+  data () {
+    return {
+      functionList: [],
+      datacollection: {
+        labels: ['赤', '青', '黄色', '緑', '紫', '橙'],
+        datasets: [
+          {
+            label: '得票数',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    }
+  },
+  
+  mounted() {
+    const directoryPath = 'C:/Users/ma17123/source/repos/syuuron/syuuron/XML/EvaluationResult/';
+    
+    const getFunctionInformationListFormOfJson = async (DirectoryPath) => {
+      const filePathList = await util.promisify(fs.readdir)(directoryPath)
+                                     .then(Pathlist => Pathlist.map(path => directoryPath + path));
+      const functionInformationList = []; 
+      for(let key in filePathList) {
+          await util.promisify(fs.readFile)(filePathList[key], {encoding : 'utf8'})
+                    .then(xmlFileData => functionInformationList.push(parser.xml2json(xmlFileData)))
+                    .catch(err => console.log(err));
+      }
+      this.functionList = functionInformationList;
+      console.table(this.functionList);
+      return functionInformationList;
+    };
+    
+    getFunctionInformationListFormOfJson(directoryPath);
+    
   },
 }
+
 </script>
 
 <style>
-html, body, main {
+html, body, sideBar {
   height: 100%;
 }
-main {
+sideBar {
   display: flex;
   justify-content: center;
   align-items: center;
